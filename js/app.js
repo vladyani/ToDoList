9fd480b9ebfab71b9d3d20d1f9694787dc39817d
Marta Radziszewska <marta.radziszewskaa@gmail.com>
2018-07-04 21:55:29 +0200

    task #2 - added form in index file, remove jQuery, changes in main-page styles, added functions in js

--- js/app.js
+++ js/app.js
@@ -1,14 +1,34 @@
 
 import "../scss/main.scss";
 
-$(function(){
    
     //tutaj umieszczamy nasz kod zgodznie z poleceniami
-    const startButton = $(".begin-btn");
-	startButton.on("click", function(e) {
+    const startButton = document.querySelector(".start-btn");
+	const mainPage = document.querySelector(".main-page"); 
+	const form = document.querySelector(".form");
+	const addNoteButton = document.querySelector(".add-note-btn");
+
+
+	startButton.addEventListener("click", function() {
+		console.log("Clik");
+		mainPage.style.display = "none";
+		showForm();
+
 		
 		
 	})
+	addNoteButton.addEventListener("click", function() {
+		console.log("add note button");
+	})
+
+
+
+	const showForm = function() {
+		form.style.display = "block";
+	}
+	const hideForm = function() {
+		form.style.display = "none";
+	}
+
+    hideForm(); // domyślne ukrycie formularza
     
-    
-});
