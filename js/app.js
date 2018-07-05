8dc89bdd77bb73fea5f7d7a8bf71a72b8aa4a726
Vlad Yantsov <vladyslavyantsov@gmail.com>
2018-07-05 14:37:12 +0100

    html update: output, dataset added , sass update: styles form finished, js update: validate form function added

--- js/app.js
+++ js/app.js
@@ -1,34 +1,62 @@
-
 import "../scss/main.scss";
 
-   
-    //tutaj umieszczamy nasz kod zgodznie z poleceniami
-    const startButton = document.querySelector(".start-btn");
-	const mainPage = document.querySelector(".main-page"); 
-	const form = document.querySelector(".form");
-	const addNoteButton = document.querySelector(".add-note-btn");
+document.addEventListener("DOMContentLoaded", function () {
 
 
-	startButton.addEventListener("click", function() {
-		console.log("Clik");
-		mainPage.style.display = "none";
-		showForm();
-
-		
-		
-	})
-	addNoteButton.addEventListener("click", function() {
-		console.log("add note button");
-	})
+    const startButton = document.querySelector(".start-btn"),
+        mainPage = document.querySelector(".main-page"),
+        form = document.querySelector(".form"),
+        addNoteButton = document.querySelector(".add-note-btn"),
+        subject = form.querySelector('[name="subject"]'),
+        date = form.querySelector('[name="deadline"]'),
+        category = form.querySelector('[name="category"]'),
+        description = form.querySelector('[name="description"]'),
+        fieldsToValidate = [...form.querySelectorAll("[required]")];
 
 
 
-	const showForm = function() {
-		form.style.display = "block";
-	}
-	const hideForm = function() {
-		form.style.display = "none";
-	}
-
+    const hideForm = function () {
+        form.style.display = "none";
+    };
     hideForm(); // domyślne ukrycie formularza
-    
+
+
+    const showForm = function () {
+        form.style.display = "block";
+    };
+
+
+
+    const validateForm = function () {
+
+        let sibling = this.nextElementSibling;
+
+        if (this.value === "" || this.value.legth < 3) {
+            sibling.textContent = this.dataset.error;
+            return false;
+        } else {
+            sibling.textContent = "";
+            return true;
+        }
+    };
+
+
+    const addNote = function () {
+        
+
+    };
+
+
+    startButton.addEventListener("click", function () {
+        mainPage.style.display = "none";
+        showForm();
+    }, false);
+
+
+    //validation for each field
+    fieldsToValidate.forEach(field => field.addEventListener('blur', validateForm, false));
+    //adding new note
+    addNoteButton.addEventListener("click", addNote, false);
+
+
+});
